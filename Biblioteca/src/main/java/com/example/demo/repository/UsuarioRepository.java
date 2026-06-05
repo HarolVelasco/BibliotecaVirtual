6a415b67f236c5c92aceddf995c67a1e0ecbc71a
HarolVelasco <velascofuentesh@gmail.com>
2026-06-05 09:09:25 -0500

    Delete Biblioteca directory

--- Biblioteca/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.repository;
-
-import com.biblioteca.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByUsername(String username);
-}
